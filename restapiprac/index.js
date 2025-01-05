588ed77babaa2f33428be1463798e68cbc5c0944
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-05 22:42:26 +0530

    something

--- restapiprac/index.js
+++ restapiprac/index.js
@@ -1,18 +1,31 @@
 const express = require("express")
+const fs = require("fs")
 const app = new express();
 const users = require('./MOCK_DATA.json')
 const port = 8000;
+
+app.use(express.urlencoded({extended:false}))
+
 app.get("/api/users",(req,res)=>{return res.json(users)})
+
+app.post("/api/users",(req,res)=>{
+    const body = req.body
+    users.push({...body,id:users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status : "success",id:users.length+1})
+    })
+})
+
 app.get("/api/users/:id",(req,res)=>{
     const id =Number(req.params.id)
     const user = users.find((user)=>user.id === id)
     return(res.json(user))
 })
-// app.get("/users",(req,res)=>{
-//     const html = `
-//     <ul>
-//     ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
-//     </ul>`
-//     res.send(html)
-// })
+ app.get("/users",(req,res)=>{
+     const html = `
+     <ul>
+     ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+     </ul>`
+    res.send(html)
+ })
 app.listen(port,()=>console.log("server is started"))
