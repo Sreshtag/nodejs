f038aea94b8d72c3c6fa1a446c9fecf4549b9542
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-11 23:44:49 +0530

    daily

---
+++ reqres/xhr.js
@@ -0,0 +1,13 @@
+const getbtn = document.getElementById('get-btn')
+const postbtn = document.getElementById('post-btn')
+
+const getdata = ()=>{
+    const xhr = XMLHttpRequest()
+    xhr.open('GET',)
+}
+const postdata = ()=>{
+
+}
+
+getbtn.addEventListner('click',getdata)
+postbtn.addEventListner('click',postdata)
