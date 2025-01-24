09d5d1fe94a1a9b40f25512a56c2126c914fa7ad
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-24 21:31:16 +0530

    server

---
+++ spreadsheetserver/index.js
@@ -0,0 +1,78 @@
+// Simple Express server setup to serve for local testing/dev API server
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const session = require('express-session');
+const jsforce = require('jsforce');
+require('dotenv').config();
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+const HOST = process.env.API_HOST || 'localhost';
+const PORT = process.env.API_PORT || 3001;
+const {
+    SALESFORCE_LOGIN_DOMAIN,
+    SALESFORCE_CLIENT_ID,
+    SALESFORCE_CLIENT_SECRET,
+    SALESFORCE_CALLBACK_URL,
+    NODE_SESSION_SECRET_KEY
+} = process.env;
+
+
+
+const oauth2 = new jsforce.OAuth2({
+    loginUrl: SALESFORCE_LOGIN_DOMAIN,
+    clientId: SALESFORCE_CLIENT_ID,
+    clientSecret: SALESFORCE_CLIENT_SECRET,
+    redirectUri: SALESFORCE_CALLBACK_URL
+});
+
+
+function getSession(req, res) {
+    const { session } = req;
+    if (session.sfdcAccessToken === undefined) {
+        res.status(401).send('Unauthorized');
+        return null;
+    }
+    return session;
+}
+
+app.use(
+    session({
+        secret: NODE_SESSION_SECRET_KEY,
+        cookie: { secure: 'auto' },
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.get('/', (req, res) => {
+    res.json({ success: true });
+});
+
+// Hook up REST endpoints with service calls
+
+// Login to Salesforce
+app.get('/oauth2/login', (req, res) => {
+    res.redirect(oauth2.getAuthorizationUrl({ scope : 'api' }));
+});
+
+// Callback function to get Auth Token
+app.get('/oauth2/callback',async(req, res) => {
+  const conn = new jsforce.Connection({ oauth2 : oauth2 });
+  const code = req.query.code;
+  const userInfo = await conn.authorize(code)
+  req.session.sfdcAccessToken = conn.accessToken;
+  req.session.sfdcInstanceUrl = conn.instanceUrl;
+  //res.json({"Access Token : ":conn.accessToken,"Instance url: ":conn.instanceUrl,"User ID: " : userInfo.id,"Org ID: " :userInfo.organizationId});
+  res.send("Logged in as " + userInfo.id + " on instance "+conn.instanceUrl)
+});
+
+
+app.listen(PORT, () =>
+    console.log(
+        `✅  API Server started: http://${HOST}:${PORT}`
+    )
+);
